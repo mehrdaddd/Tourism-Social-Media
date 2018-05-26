749c0e3564bc973033417d4fa117be7fb6f761cb
Mehrdad Saraee <mehrdad.saraee@gmail.com>
2018-05-26 11:23:13 +0200

    UI

---
+++ src/22.js
@@ -0,0 +1,14 @@
+<div className="App">
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <PageHeader>
+            Tourism
+        </PageHeader>
+    </header>
+    <div className="container">
+        <h1>
+            The best Tourism Companies
+        </h1>
+    </div>
+
+</div>
