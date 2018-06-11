f09a6e35f2b72a758a6f364a805eb4acac922cfe
unknown <mehrdad.saraee@gmail.com>
2018-06-11 19:42:11 +0200

    star

--- src/components/Star.js
+++ src/components/Star.js
@@ -6,41 +6,42 @@ const rchange = (value) => {
     console.log(value)
     const rootstarr = app.database().ref().child('app').child('panel').child('items').child('star');
     rootstarr.push().set(value);
+    alert( "Your Rating is saved");
 }
 
 class Star extends Component {
     constructor(){
         super();
         this.state = {
-            rate:0,
-            orate:[],
-            i:0
-
+            orate:0
         };
     }
 
     componentDidMount() {
         const rootstar = app.database().ref().child('app').child('panel').child('items').child('star');
 
-
-        rootstar.on('child_added', snap => {
-
-            this.state.i +=1 ;
-            this.state.rate += snap.val();
-            this.setState({    });
+        var i=0;
+        var rate=0;
+        var orate=0;
+        rootstar.on('child_added', snap  => {
+            i +=1 ;
+            rate += snap.val();
+            orate=rate/i;
+                this.setState({
+                    orate: orate
+                });
         });
 
     }
 
     render() {
-        console.log(this.state.i);
-        console.log(this.state.rate);
+
 return(
 
 <ReactStars className="star"
     count={5}
     size={30}
-    value={this.state.rate}
+    value={this.state.orate}
     color2={'#ffd700'}
     color1={'black'}
     half={false}
